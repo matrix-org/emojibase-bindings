2ee3151cc801a056702fa2fc821423f833f975ae
David Langley <langley.dave@gmail.com>
2023-08-09 22:29:10 +0100

    Run iOS tests from the correct directory

--- platforms/android/library/src/test/java/io/element/android/emojibasebindings/EmojibaseUnitTest.kt
+++ platforms/android/library/src/test/java/io/element/android/emojibasebindings/EmojibaseUnitTest.kt
@@ -10,24 +10,4 @@ import org.junit.Before
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class EmojibaseUnitTest {
-
-    lateinit var store: EmojibaseStore
-
-    @Before
-    fun setUp() {
-        store = EmojibaseDatasource().load()
-    }
-
-    @Test
-    fun testSpecificEmoji() {
-        assertEquals(store.categories.size, EmojibaseCategory.values().size)
-        val people = store.emojisFor(EmojibaseCategory.People)
-        assertNotNull(people)
-        val emoji = people?.first { it.label == "OK hand" }
-        assertEquals(emoji?.tags, listOf("hand", "ok"))
-        assertEquals(emoji?.shortcodes, listOf("ok_hand"))
-        assertEquals(emoji?.skins?.size, 5)
-        assertEquals(emoji?.skins?.last()?.unicode, "👌🏿")
-    }
-}
+class EmojibaseUnitTest {}
