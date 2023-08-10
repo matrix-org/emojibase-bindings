704131c44692e3c425c37d56e4ca8a60dfc40939
David Langley <langley.dave@gmail.com>
2023-08-10 17:45:26 +0100

    Fix paths

--- scripts/generateJson.sh
+++ scripts/generateJson.sh
@@ -5,8 +5,8 @@ set -e
 yarn start
 
 EMOJIJSON_FILE="build/emojibase.json"
-SWIFT_PATH="platforms/ios/Emojibase/Resources/emojibase.json"
-KOTLIN_PATH="platforms/android/library/src/main/assets/emojibase.json"
+SWIFT_PATH="platforms/ios/Emojibase/Resources"
+KOTLIN_PATH="platforms/android/library/src/main/assets"
 
 cp "$EMOJIJSON_FILE" "$SWIFT_PATH"
 mkdir -p "$KOTLIN_PATH" && cp "$EMOJIJSON_FILE" "$KOTLIN_PATH"
